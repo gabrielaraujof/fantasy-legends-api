89620abc0384c177e941a19d235ea823da5dafe1
Gabriel Araujo <git@gabrielaraujo.dev>
2023-07-03 23:39:37 -0300

    feat: add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      imports: [ConfigModule],
+      driver: ApolloDriver,
+      useFactory: (config: ConfigService) => ({
+        playground: config.get('PLAYGROUND'),
+        autoSchemaFile: true,
+        sortSchema: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
---
+++ src/app.resolver.ts
@@ -0,0 +1,12 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { AppService } from './app.service';
+
+@Resolver()
+export class AppResolver {
+  constructor(private readonly service: AppService) {}
+
+  @Query(() => String)
+  sayHello(): string {
+    return this.service.getHello();
+  }
+}
